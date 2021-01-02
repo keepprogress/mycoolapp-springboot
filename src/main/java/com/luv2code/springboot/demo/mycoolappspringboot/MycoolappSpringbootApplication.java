f89b5f5b01ed0c4316ba15b1be6e9d5deeb1c2f8
getzch <keepprogress@gmail.com>
2021-01-03 01:25:45 +0800

    import from zip which is generated by spring Initializr

---
+++ src/main/java/com/luv2code/springboot/demo/mycoolappspringboot/MycoolappSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.demo.mycoolappspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycoolappSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycoolappSpringbootApplication.class, args);
+	}
+
+}
